e52195fe515a07b38ff178348aea328e6a1d8968
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-11 17:49:15 +0200

    2.5.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,15 +2,23 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-//        TextBox textBox1 = new TextBox(); // class instantiating
-        var textBox1 = new TextBox();
+        var textBox1 = new TextBox(); // textBox1 references an object on the HEAP
+        /*
+        Java memory allocation process:
+        1. Evaluate 'new TextBox()' -> create new object and store it on the HEAP
+        2. Execute 'var textBox1' -> allocate memory on the STACK and store object's address in it
+         */
 
-        System.out.println(textBox1.text); // not instantiated reference types return 'null'
-        textBox1.setText("Box 1");
-        System.out.println(textBox1.text);
+        var textBox2 = textBox1; // 2x variables on the STACK and 1x object on the HEAP
 
-        var textBox2 = new TextBox();
-        textBox2.setText("Box 2"); // objects are independent of each other
+        // both variables are referencing the same object
+        textBox1.setText("Hello World");
         System.out.println(textBox2.text);
+
+        /*
+        Java memory deallocation process:
+        1. Remove variables from the STACK
+        2. Garbage Collector:  (if there are no references to the object) Remove object from the HEAP
+         */
     }
 }
