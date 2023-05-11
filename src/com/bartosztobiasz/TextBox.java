c96dd4dff39416f89738052a68e3d20d878f4ef1
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-11 17:06:39 +0200

    2.3.1

---
+++ src/com/bartosztobiasz/TextBox.java
@@ -0,0 +1,18 @@
+package com.bartosztobiasz;
+
+// access_modifier class PascalCase
+public class TextBox {
+    // fields:
+//    public String text;
+    public String text = "";
+
+    // methods:
+    public void setText(String text) {
+        // use 'this' keyword when the parameter name is the same as the field name
+        this.text = text; // 'this' -> reference to the current object
+    }
+
+    public void clear() {
+        text = "";
+    }
+}
