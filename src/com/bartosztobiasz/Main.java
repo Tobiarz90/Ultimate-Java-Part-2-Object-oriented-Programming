2de47c66a5aa99ca596a4a36f9b1c987c0bedc41
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-11 17:59:47 +0200

    2.6.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,23 +2,23 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        var textBox1 = new TextBox(); // textBox1 references an object on the HEAP
-        /*
-        Java memory allocation process:
-        1. Evaluate 'new TextBox()' -> create new object and store it on the HEAP
-        2. Execute 'var textBox1' -> allocate memory on the STACK and store object's address in it
-         */
+        // Procedural paradigm is based on procedure/function cost
 
-        var textBox2 = textBox1; // 2x variables on the STACK and 1x object on the HEAP
+        int baseSalary = 50_000;
+        int extraHours = 10;
+        int hourlyRate = 20;
 
-        // both variables are referencing the same object
-        textBox1.setText("Hello World");
-        System.out.println(textBox2.text);
+        int wage = calculateWage(baseSalary, extraHours, hourlyRate);
+        System.out.println(wage);
+    }
 
-        /*
-        Java memory deallocation process:
-        1. Remove variables from the STACK
-        2. Garbage Collector:  (if there are no references to the object) Remove object from the HEAP
-         */
+    // procedure
+    public static int calculateWage(
+            // a lot of parameters
+            int baseSalary,
+            int extraHours,
+            int hourlyRate
+    ) {
+        return baseSalary + (extraHours * hourlyRate);
     }
 }
