067fa8bfdb3124906a0d11fb13f8336c10ed4716
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-12 20:13:06 +0200

    3.7.1

---
+++ src/com/bartosztobiasz/MortgageCalculator.java
@@ -0,0 +1,41 @@
+package com.bartosztobiasz;
+
+// calculations class
+public class MortgageCalculator {
+    private int principal;
+    private float annualInterest;
+    private byte years;
+
+    /*
+    calculateBalance() and calculateMortgage() are using the same set of parameters:
+    int principal, float annualInterest, byte years
+
+    You should store them inside the fields of a class -> encapsulation
+    and initialize them in a constructor.
+     */
+    public MortgageCalculator(int principal, float annualInterest, byte years) {
+        this.principal = principal;
+        this.annualInterest = annualInterest;
+        this.years = years;
+    }
+
+    public double calculateMortgage() {
+        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
+        float numberOfPayments = years * Main.MONTHS_IN_YEAR;
+
+        return principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+    }
+
+    public double calculateBalance(
+            short numberOfPaymentsMade
+    ) {
+        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
+        float numberOfPayments = years * Main.MONTHS_IN_YEAR;
+
+        return principal
+                * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+    }
+}
--- src/com/bartosztobiasz/MortgageReport.java
+++ src/com/bartosztobiasz/MortgageReport.java
@@ -5,8 +5,11 @@ import java.text.NumberFormat;
 // presentation class
 // this class manages all presentation concerns (printing)
 public class MortgageReport {
+    private static MortgageCalculator calculator;
+
     public static void printMortgage(int principal, float annualInterest, byte years) {
-        double mortgage = Main.calculateMortgage(principal, annualInterest, years);
+        calculator = new MortgageCalculator(principal, annualInterest, years);
+        double mortgage = calculator.calculateMortgage();
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println();
         System.out.println("MORTGAGE");
@@ -14,12 +17,12 @@ public class MortgageReport {
         System.out.println("Monthly Payments: " + mortgageFormatted);
     }
 
-    public static void printPaymentSchedule(int principal, float annualInterest, byte years) {
+    public static void printPaymentSchedule(byte years) {
         System.out.println();
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("----------------");
         for (short month = 1; month <= years * Main.MONTHS_IN_YEAR; month++) {
-            double balance = Main.calculateBalance(principal, annualInterest, years, month);
+            double balance = calculator.calculateBalance(month);
             System.out.println(NumberFormat.getCurrencyInstance().format(balance));
         }
     }
