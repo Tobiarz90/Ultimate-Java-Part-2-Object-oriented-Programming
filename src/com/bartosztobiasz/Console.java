011bba44779d95057b0a2b748fea875bbb474b31
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-12 19:49:28 +0200

    3.5.1

--- src/com/bartosztobiasz/Console.java
+++ src/com/bartosztobiasz/Console.java
@@ -3,8 +3,15 @@ package com.bartosztobiasz;
 import java.util.Scanner;
 
 public class Console {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static double readNumber(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextDouble();
+    }
+
+    // static -> because we don't have multiple consoles in console applications
     public static double readNumber(String prompt, double min, double max) {
-        Scanner scanner = new Scanner(System.in);
         double value;
         while (true) {
             System.out.print(prompt);
