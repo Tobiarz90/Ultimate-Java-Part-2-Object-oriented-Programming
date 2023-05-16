95a5300978169672100062b9eb82ba1d045489e1
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-16 13:56:47 +0200

    4.7.2

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,14 +2,18 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        var control = new UIControl(true);
-        show(control);
-
         var textBox = new TextBox();
         show(textBox); // upcasting; textBox object gets automatically cast to UIControl object
     }
 
     public static void show(UIControl control) {
+        /*
+        at compile time we can only work with the members (methods, fields) of the UIControl class unless...
+        explicit casting
+         */
+        var textBox = (TextBox) control; // downcasting
+
+        textBox.setText("Hello World");
         System.out.println(control);
     }
 }
