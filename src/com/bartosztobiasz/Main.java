5754c5869387b32e1180f2c73665f61c760999ee
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-16 14:32:32 +0200

    4.8.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,35 +2,10 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        var control = new UIControl(true);
-        show(control);
+        var point1 = new Point(1, 2);
+        var point2 = new Point(1, 2);
 
-        var textBox = new TextBox();
-        show(textBox); // upcasting
-    }
-
-    public static void show(UIControl control) {
-        /*
-        at compile time we can only work with the members (methods, fields) of the UIControl class unless...
-        explicit casting
-         */
-
-        /*
-        every TextBox is a UIControl
-
-        not every UIControl is a TextBox:
-        (TextBox) UIControl
-        (TextBox) Button
-        (TextBox) List
-        ...
-         */
-
-        // ClassCastException -> casting generic type to a more specialized type
-        if (control instanceof TextBox) {
-            var textBox = (TextBox) control; // (TextBox) TextBox
-            textBox.setText("Hello World");
-        }
-
-        System.out.println(control);
+        System.out.println(point1 == point2); // comparing addresses
+        System.out.println(point1.equals(point2)); // comparing based on references
     }
 }
