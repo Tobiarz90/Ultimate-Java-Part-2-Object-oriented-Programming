333e824337a153e83e4331269c7fa33c19d476a5
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-16 12:05:05 +0200

    4.2.2

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,7 +2,7 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        var control = new UIControl();
+        var control = new TextBox();
         control.disable();
         System.out.println(control.isEnabled());
     }
