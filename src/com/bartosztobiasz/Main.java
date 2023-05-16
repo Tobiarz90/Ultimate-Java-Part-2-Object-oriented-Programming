a2f9af0191082764748877e30c579c0c0d0c9d00
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-16 12:14:34 +0200

    4.3.2

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,7 +2,19 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        var object = new Object();
-        System.out.println(object.hashCode());
+        var box1 = new TextBox();
+//        var box2 = box1;
+        var box2 = new TextBox();
+
+        System.out.println(box1.hashCode()); // hashed code calculated based on the address of this object
+        System.out.println(box2.hashCode());
+
+        System.out.println(box1.equals(box2)); // by default: compares two objects by their hash codes
+
+        // Returns a string representation of the object.
+        // fully.qualified.name.of.the.class@hashCodeRepresentedAsHexodecimal
+        System.out.println(box1.toString());
+
+        // Changing the implementation of the equal()/toString() method -> method overriding.
     }
 }
