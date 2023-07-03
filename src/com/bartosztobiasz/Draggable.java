d4ec1f9fd139968696964dbe98dc197d0e6895ec
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-03 13:59:37 +0200

    5.10.1

--- src/com/bartosztobiasz/Draggable.java
+++
@@ -1,6 +0,0 @@
-package com.bartosztobiasz;
-
-// One interface = one capability
-public interface Draggable {
-    void drag();
-}
--- src/com/bartosztobiasz/UIWidget.java
+++
@@ -1,7 +0,0 @@
-package com.bartosztobiasz;
-
-public interface UIWidget
-        extends Draggable, Resizable // multiple parents
-{
-    void render();
-}
