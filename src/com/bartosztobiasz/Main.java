0893877bb36a865d4fd461b47093592cddc37b0d
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-03 11:59:56 +0200

    5.8.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,15 +2,11 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        var calculator = new TaxCalculator2023(100_000); // dependency
-        var report = new TaxReport(calculator); // (concrete implementation)
-        report.show();
+        var report = new TaxReport();
 
-        /*
-        Benefit of setter injection:
-        implementation of dependency can be changed throughout the lifetime of a program
-         */
-        report.setCalculator(new TaxCalculator2024()); // setter(concrete implementation)
-        report.show();
+        var calculator = new TaxCalculator2023(100_000); // dependency
+        report.show(calculator);
+
+        report.show(new TaxCalculator2024());
     }
 }
--- src/com/bartosztobiasz/TaxReport.java
+++ src/com/bartosztobiasz/TaxReport.java
@@ -1,20 +1,9 @@
 package com.bartosztobiasz;
 
 public class TaxReport {
-    private TaxCalculator calculator;
-
-    // Constructor injection
-    public TaxReport(TaxCalculator calculator) {
-        this.calculator = calculator;
-    }
-
-    public void show() {
+    // Method injection
+    public void show(TaxCalculator calculator) {
         var tax = calculator.calculateTax();
         System.out.println(tax);
-    }
-
-    // Setter injection
-    public void setCalculator(TaxCalculator calculator) {
-        this.calculator = calculator;
     }
 }
